e3aab44e7252a014efd33d8b1c56724a0053f588
Tony Lim <atomictheorist@gmail.com>
2017-04-24 00:43:09 -0700

    bower heroku deploy script

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
 
 app.use('/', index);
 app.use('/users', users);
