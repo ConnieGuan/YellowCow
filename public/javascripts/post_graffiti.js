15e1163a35dd1d8ce4c69c40672ccbce8baf9cb9
Chang <cgwoun21@gmail.com>
2017-05-03 08:27:51 -0700

    made https request changes

--- public/javascripts/post_graffiti.js
+++ public/javascripts/post_graffiti.js
@@ -11,7 +11,7 @@ var map = L.map('map', {
     ).locate({setView: true, maxZoom: 16});
 
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
