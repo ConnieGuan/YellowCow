c41b7f414c8f87c48abfd5ceadd4a83516421191
Jorenne Flores <jjflores@ucsd.edu>
2017-05-09 22:02:49 -0700

    redesigned error page and fixed graphics

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('404 NOT FOUND');
   err.status = 404;
   next(err);
 });
