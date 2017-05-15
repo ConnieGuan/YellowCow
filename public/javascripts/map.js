1751cb11a442d620f71672a12e7721d1e094e045
Tony Lim <atomictheorist@gmail.com>
2017-05-14 21:55:30 -0700

    (test) enable other stuffs beside lambda

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -7,30 +7,12 @@ $(document).ready(function (event) {
     /**
      * set up data from server before showing the map view
      */
-    // var data;
-    // $.get('/api/data', {}, function (res, req) {
-    //     data = res;
-    //     return res;
-    // }).done(setupMap);
-    setupSimpleMap();
+    var data;
+    $.get('/api/data', {}, function (res, req) {
+        data = res;
+        return res;
+    }).done(setupMap);
 });
-
-/**
- * For debug in mobile web
- */
-function setupSimpleMap() {
-    var ucsd_coor = [32.87822, -117.24152];
-
-    var map = L.map('map', {
-        center: ucsd_coor,
-        zoom: 15,
-        doubleClickZoom: false
-    }).locate({setView: true, maxZoom: 16});
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-}
 
 /**
  * method to set up the map interfaces
