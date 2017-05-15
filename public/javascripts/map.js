53a01f6c525e15a6d1f300c8447c5684fa85f25b
Tony Lim <atomictheorist@gmail.com>
2017-05-14 21:26:20 -0700

    (mobile test) disable ajax map

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -8,12 +8,29 @@ $(document).ready(function (event) {
      * set up data from server before showing the map view
      */
     var data;
-    $.get('/api/data', {}, function (res, req) {
-        data = res;
-        return res;
-    }).done(setupMap);
-
+    // $.get('/api/data', {}, function (res, req) {
+    //     data = res;
+    //     return res;
+    // }).done(setupMap);
+    setupSimpleMap();
 });
+
+/**
+ * For debug in mobile web
+ */
+function setupSimpleMap() {
+    var ucsd_coor = [32.87822, -117.24152];
+
+    var map = L.map('map', {
+        center: ucsd_coor,
+        zoom: 15,
+        doubleClickZoom: false
+    }).locate({setView: true, maxZoom: 16});
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+}
 
 /**
  * method to set up the map interfaces
@@ -193,7 +210,7 @@ function setupMap(data) {
 
 
 //    useful to add new sample coordinate data later
-    map.on('click', function(e) {
-        console.log(e.latlng.lng + ',' + e.latlng.lat);
-    });
+//     map.on('click', function(e) {
+//         console.log(e.latlng.lng + ',' + e.latlng.lat);
+//     });
 }
