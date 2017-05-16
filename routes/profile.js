6fabf9ee40ae72f9550b7067a4c10b2e11dcf2dd
Jorenne Flores <jjflores@ucsd.edu>
2017-05-15 21:29:00 -0700

    profile shows graffiti and also updated error page

--- routes/profile.js
+++ routes/profile.js
@@ -3,10 +3,11 @@
  */
 var express = require('express');
 var router = express.Router();
+var data = require('../data.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('profile', { title: 'Profile' });
+    res.render('profile', { title: 'Profile', 'data': data });
 });
 
 module.exports = router;
